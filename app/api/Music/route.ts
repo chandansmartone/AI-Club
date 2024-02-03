bdaf9b34ad9b7c9af9ce5c0599df935f1e068ad0
chandu <chandan03chandu@gmail.com>
2024-02-03 23:56:00 +0530

    added copy button and changed music API

--- app/api/Music/route.ts
+++ app/api/Music/route.ts
@@ -35,8 +35,18 @@ export async function POST(
       "meta/musicgen:b05b1dff1d8c6dc63d14b0cdb42135378dcb87f6373b0d3d341ede46e59e2b38",
       {
         input: {
-          model_version: "stereo-melody-large",
-          prompt:prompt
+          top_k: 250,
+          top_p: 0,
+          prompt: prompt,
+          duration: 15,
+          temperature: 1,
+          continuation: false,
+          model_version: "stereo-large",
+          output_format: "wav",
+          continuation_start: 0,
+          multi_band_diffusion: false,
+          normalization_strategy: "peak",
+          classifier_free_guidance: 3
         }
       }
     );
