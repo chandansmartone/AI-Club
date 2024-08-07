483c1f1c136ef8ad027fbe6f3d1b017a81556452
chandansmartone <chandan03chandu@gmail.com>
2024-08-07 13:36:55 +0530

    changed icon

--- app/api/code/route.ts
+++ app/api/code/route.ts
@@ -41,8 +41,8 @@ export async function POST(req: Request) {
   
      
       const response = await openai.chat.completions.create({
-        // model: "gpt-3.5-turbo",
-        model: "gpt-4",
+        model: "gpt-3.5-turbo",
+        // model: "gpt-4",
         messages: [instructionMessage, ...messages]
       });
   
